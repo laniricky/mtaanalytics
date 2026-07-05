e0b2b9962515fb498b06eb574f5e20c704b51678
Fredrick Lani <fredricklani@gmail.com>
2026-07-05 08:15:20 +0300

    Fix: add missing call import for Ktor 2.x route handler

--- backend/src/main/kotlin/com/mtaanimation/growthos/backend/plugins/Routing.kt
+++ backend/src/main/kotlin/com/mtaanimation/growthos/backend/plugins/Routing.kt
@@ -14,6 +14,7 @@ import com.mtaanimation.growthos.backend.routes.customGoalRoutes
 import com.mtaanimation.growthos.backend.services.ProjectionService
 import io.ktor.http.ContentType
 import io.ktor.server.application.Application
+import io.ktor.server.application.call
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
